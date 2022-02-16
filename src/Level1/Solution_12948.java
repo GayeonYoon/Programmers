16ecbb8a74c92b0e450b5e9e7c747b47fbaa61cb
GayeonYoon <1226betty@naver.com>
2022-02-16 22:15:16 +0900

    2022.02.16 commit

--- src/Level1/Solution_12948.java
+++ src/Level1/Solution_12948.java
@@ -2,14 +2,19 @@ package Level1;
 
 //핸드폰 번호 가리기
 public class Solution_12948 {
+	
 	public static void main(String[] args) {
-		
 		String phone_number = "01033334444";
 		String answer = "";
-		
+
 		for (int i = 0; i < phone_number.length(); i++) {
-			answer += (phone_number.length() - i) <= 4 ? phone_number.charAt(i) : '*';
+			if ((phone_number.length() - i) <= 4) {
+				answer += phone_number.charAt(i);
+			} else {
+				answer += '*';
+			}
 		}
+
 	}
 
 }
