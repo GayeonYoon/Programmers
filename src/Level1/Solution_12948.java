ecdeeb13950892bb88c801ba0f20ac2c25bccfef
GayeonYoon <1226betty@naver.com>
2022-02-16 22:17:26 +0900

    2022.02.16 commit

--- src/Level1/Solution_12948.java
+++ src/Level1/Solution_12948.java
@@ -6,13 +6,9 @@ public class Solution_12948 {
 	public static void main(String[] args) {
 		String phone_number = "01033334444";
 		String answer = "";
-
+		
 		for (int i = 0; i < phone_number.length(); i++) {
-			if ((phone_number.length() - i) <= 4) {
-				answer += phone_number.charAt(i);
-			} else {
-				answer += '*';
-			}
+			answer += (phone_number.length() - i) <= 4 ? phone_number.charAt(i) : '*';
 		}
 
 	}
