8139ccc7d9be38c6e1794110b338a1f7435d438f
GayeonYoon <1226betty@naver.com>
2022-02-21 17:45:50 +0900

    2022.02.21 commit

---
+++ src/Level1/Solution_12903.java
@@ -0,0 +1,23 @@
+package Level1;
+
+public class Solution_12903 {
+
+	public static void main(String[] args) {
+		String s = "abcde";
+		Solution_12903 sol = new Solution_12903();
+		sol.solution(s);
+	}
+
+	public String solution(String s) {
+		String answer = "";
+		int start_index = s.length() / 2;
+
+		if (s.length() % 2 == 0) { // 짝수
+			answer += s.substring(start_index - 1, start_index + 1);
+		} else { // 홀수
+			answer += s.substring(start_index, start_index + 1);
+		}
+
+		return answer;
+	}
+}
